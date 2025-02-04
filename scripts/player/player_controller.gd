9bf061c516de7aab58cf9c131c1e79d5d3bf20bb
Paul Sparks <psparks1225@gmail.com>
2025-02-03 23:37:44 -0500

    add crouching

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 class_name PlayerController
 
-const SPEED = 4.5
+const WALK_SPEED = 4.5
+const CROUCH_SPEED = 2.5
 const JUMP_VELOCITY = 7.0
 const SENSITIVITY = 0.003
 const BOB_FREQ = 3.0
@@ -10,14 +11,18 @@ const GRAVITY = 25.0
 const TERMINAL_VELOCITY = 35.0
 const REACH_DISTANCE = 3
 
+var speed = WALK_SPEED
 var t_bob := 0.0
 var current_gravity := 12.0
 var held_prop: Prop = null
 var held_prop_mesh: MeshInstance3D = null
+var can_stand: bool = true
 
 @onready var camera: Camera3D = $Camera3D
 @onready var wallet: WalletComponent = $WalletComponent
 @onready var inventory: InventoryComponent = $InventoryComponent
+@onready var top_half_1: CollisionShape3D = $TopHalf1
+@onready var top_half_2: CollisionShape3D = $TopHalf2
 
 
 func _ready():
@@ -47,8 +52,8 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forwards", "backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
 		velocity.x = 0
 		velocity.z = 0
@@ -56,9 +61,16 @@ func _physics_process(delta: float) -> void:
 
 	# Head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob) + Vector3(0,1,0)
+	camera.transform.origin = _headbob(t_bob) + (Vector3(0,1,0) if (can_stand and not Input.is_action_pressed("crouch")) else Vector3(0, 0.2, 0))
 
 	move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("crouch"):
+		top_half_1.disabled = true
+		top_half_2.disabled = true
+		speed = CROUCH_SPEED
 
 
 func _headbob(time) -> Vector3:
@@ -69,8 +81,12 @@ func _headbob(time) -> Vector3:
 
 
 func _process(_delta):
+	if can_stand and not Input.is_action_pressed("crouch"):
+		top_half_1.disabled = false
+		top_half_2.disabled = false
+		speed = WALK_SPEED
 	_handle_raycast()
-
+	
 
 func _handle_raycast() -> void:
 	var space_state = get_world_3d().direct_space_state
@@ -110,3 +126,13 @@ func _handle_raycast() -> void:
 func _handle_prop_pickup(prop: Prop) -> void:
 		held_prop = prop
 		held_prop.freeze = true
+
+
+func _on_top_half_area_body_entered(body: Node3D) -> void:
+	if not body is PlayerController:
+		can_stand = false
+
+
+func _on_top_half_area_body_exited(body: Node3D) -> void:
+	if not body is PlayerController:
+		can_stand = true
