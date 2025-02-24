333cf873b38b233f62549449a96198b5e954703d
paulsparks <psparks1225@gmail.com>
2025-02-23 19:55:03 -0500

    add tooltips to items and prices

---
+++ lib/helper_functions.gd
@@ -0,0 +1,13 @@
+class_name HelperFunctions
+
+
+## Find a node's child using a filter function that takes a child node and returns true or false.
+## Returns the first node found, or null if none are found.
+static func find_child_with_func(node: Node, filter: Callable) -> Node:
+	for child: Node in node.get_children():
+		var child_matches_condition: bool = filter.call(child)
+
+		if child_matches_condition:
+			return child
+
+	return null
