e8eb4ab305d1d2f26997f75d19cc330f765912b5
paulsparks <psparks1225@gmail.com>
2025-04-06 21:15:19 -0400

    add item icons and broken drop button

--- scripts/lib/create_item.gd
+++ scripts/lib/create_item.gd
@@ -10,6 +10,14 @@ static var item_table: Dictionary = {
 	"sprit": "food/sprit.tscn",
 }
 
+static var texture_map: Dictionary = {
+	"screwdriver": Vector2(844, 0),
+	"promethazine": Vector2(605, 0),
+	"foam_cups_24_pack": Vector2(366, 0),
+	"jolly_ranglers": Vector2(244, 0),
+	"sprit": Vector2(488, 0),
+}
+
 
 static func create_item(item_id: String) -> Item:
 	var item_string: String = ITEMS_PATH % item_table.get(item_id)
